6878aa7d210cc16ac1a50a9572a76bc155e2978d
宋培众 <spzhongwin@126.com>
2018-12-07 18:34:43 +0800

    commit

--- ftcg/models.py
+++ ftcg/models.py
@@ -5,8 +5,9 @@ from django.db import models
 
 class user(models.Model):
     name = models.CharField(max_length=255, db_index=True, unique=True)
-    code = models.CharField(max_length=32)
+    code = models.CharField(max_length=32,null=True)
     phone = models.CharField(max_length=15, null=True)
+    role = models.IntegerField(max_length=2)
     createTime = models.CharField(null=True, max_length=20)
 
 
