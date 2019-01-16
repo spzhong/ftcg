04bc7a98126183c6f6f1ea1167284f9db8c37585
宋培众 <spzhongwin@126.com>
2019-01-16 09:47:57 +0800

    commit

--- ftcg/application/config/configAdmin.py
+++ ftcg/application/config/configAdmin.py
@@ -262,7 +262,7 @@ def adminGetVillages(request):
             villageList = village.objects.filter(streetId=streetId_parm, communityId=communityId_parm)
         list = []
         for oneVillage in villageList:
-             list.append({'id': oneVillage.id, 'isOpen': oneVillage.isOpen,'name': oneVillage.name,'type': oneVillage.type,'number': oneVillage.number,
+             list.append({"streetId":streetId_parm,"communityId":oneVillage.communityId,'id': oneVillage.id, 'isOpen': oneVillage.isOpen,'name': oneVillage.name,'type': oneVillage.type,'number': oneVillage.number,
                                         'address': oneVillage.address, 'personCharge': oneVillage.personCharge,
                                         'phone': oneVillage.phone, 'remarks': oneVillage.remarks,
                                         'managementSubsetNum': oneVillage.managementSubsetNum})
